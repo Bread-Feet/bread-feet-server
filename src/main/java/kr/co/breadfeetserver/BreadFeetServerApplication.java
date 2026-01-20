c1465bed5201b4727f87ba5a370c39a776c1432f
wlgns12370 <knu12370@knu.ac.kr>
2026-01-20 14:25:21 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/kr/co/breadfeetserver/BreadFeetServerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.breadfeetserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadFeetServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreadFeetServerApplication.class, args);
+	}
+
+}
