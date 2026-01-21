0a8104d4ce8f9bd3aab8519626691c3efdd0856d
wlgns12370 <knu12370@knu.ac.kr>
2026-01-21 17:01:12 +0900

    feat: Menu 도메인 구현

---
+++ src/main/java/kr/co/breadfeetserver/domain/bakery/Menu.java
@@ -0,0 +1,35 @@
+package kr.co.breadfeetserver.domain.bakery;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import kr.co.breadfeetserver.global.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "menu")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Menu extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "menu_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(name = "bakery_id", nullable = false)
+    private Long bakeryId;
+}
