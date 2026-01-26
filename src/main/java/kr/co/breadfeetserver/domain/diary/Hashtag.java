e0620a1a2ddbaf967bf95efef826ba2eaa4b82e9
chjh1211 <chjh8800@naver.com>
2026-01-26 15:22:41 +0900

    feat: 해쉬태그, url 엔티티 분리

--- src/main/java/kr/co/breadfeetserver/domain/diary/Hashtag.java
+++ src/main/java/kr/co/breadfeetserver/domain/diary/Hashtag.java
@@ -1,4 +1,21 @@
 package kr.co.breadfeetserver.domain.diary;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table(name = "hashtag")
+@NoArgsConstructor
 public class Hashtag {
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Long diaryId;
 }
--- src/main/java/kr/co/breadfeetserver/domain/diary/PictureUrl.java
+++ src/main/java/kr/co/breadfeetserver/domain/diary/PictureUrl.java
@@ -1,4 +1,21 @@
 package kr.co.breadfeetserver.domain.diary;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table(name = "hashtag")
+@NoArgsConstructor
 public class PictureUrl {
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String pic_url;
+
+    @Column(nullable = false)
+    private Long diaryId;
 }
