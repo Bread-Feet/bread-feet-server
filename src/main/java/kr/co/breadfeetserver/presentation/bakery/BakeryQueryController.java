32055ebbd41bcf3e0095f762ef405ceb66e3571a
wlgns12370 <knu12370@knu.ac.kr>
2026-02-03 16:26:13 +0900

    feat: Bakery 목록 조회 presentation 계층 구현

---
+++ src/main/java/kr/co/breadfeetserver/presentation/bakery/BakeryQueryController.java
@@ -0,0 +1,38 @@
+package kr.co.breadfeetserver.presentation.bakery;
+
+import kr.co.breadfeetserver.application.bakery.BakeryQueryService;
+import kr.co.breadfeetserver.infra.util.ApiResponseWrapper;
+import kr.co.breadfeetserver.presentation.bakery.dto.request.BakeryCursorCommand;
+import kr.co.breadfeetserver.presentation.bakery.dto.response.BakeryListResponse;
+import kr.co.breadfeetserver.presentation.support.CursorResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/bakeries")
+public class BakeryQueryController {
+
+    private final BakeryQueryService bakeryQueryService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponseWrapper<CursorResponse<BakeryListResponse>>> getBakeryList(
+            @RequestParam(required = false, defaultValue = "0") Long cursor,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        BakeryCursorCommand command = new BakeryCursorCommand(cursor, size);
+
+        CursorResponse<BakeryListResponse> response = bakeryQueryService.getBakeryList(
+                command);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponseWrapper.success(HttpStatus.OK, "빵집 목록 조회 성공",
+                        response));
+    }
+}
