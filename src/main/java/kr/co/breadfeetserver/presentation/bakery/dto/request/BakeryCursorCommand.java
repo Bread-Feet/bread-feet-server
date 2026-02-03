25bc62674e66620786b576324fc471acc34b8ac4
wlgns12370 <knu12370@knu.ac.kr>
2026-02-03 16:25:32 +0900

    feat: Bakery 목록 조회 기능 구현

---
+++ src/main/java/kr/co/breadfeetserver/presentation/bakery/dto/request/BakeryCursorCommand.java
@@ -0,0 +1,13 @@
+package kr.co.breadfeetserver.presentation.bakery.dto.request;
+
+import jakarta.validation.constraints.Min;
+import kr.co.breadfeetserver.presentation.annotation.CursorSize;
+
+public record BakeryCursorCommand(
+        @Min(0)
+        Long cursor,
+        @CursorSize
+        int size
+) {
+
+}
