e71d7218099f4bde5c56b155479a8b2d0fc88972
wlgns12370 <knu12370@knu.ac.kr>
2026-02-03 22:15:36 +0900

    feat: Bakery 단건 조회 기능 구현

--- src/main/java/kr/co/breadfeetserver/application/bakery/BakeryQueryService.java
+++ src/main/java/kr/co/breadfeetserver/application/bakery/BakeryQueryService.java
@@ -1,8 +1,12 @@
 package kr.co.breadfeetserver.application.bakery;
 
 import kr.co.breadfeetserver.application.support.CursorService;
-import kr.co.breadfeetserver.domain.bakery.query.BakeryJdbcRepository;
+import kr.co.breadfeetserver.domain.bakery.Bakery;
+import kr.co.breadfeetserver.domain.bakery.query.BakeryQueryRepository;
+import kr.co.breadfeetserver.infra.exception.BreadFeetBusinessException;
+import kr.co.breadfeetserver.infra.exception.ErrorCode;
 import kr.co.breadfeetserver.presentation.bakery.dto.request.BakeryCursorCommand;
+import kr.co.breadfeetserver.presentation.bakery.dto.response.BakeryDetailResponse;
 import kr.co.breadfeetserver.presentation.bakery.dto.response.BakeryListResponse;
 import kr.co.breadfeetserver.presentation.support.CursorResponse;
 import lombok.RequiredArgsConstructor;
@@ -16,11 +20,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 public class BakeryQueryService {
 
-    private final BakeryJdbcRepository bakeryJdbcRepository;
+    private final BakeryQueryRepository bakeryJdbcRepository;
     private final CursorService cursorService;
 
     public CursorResponse<BakeryListResponse> getBakeryList(BakeryCursorCommand command) {
         final Slice<BakeryListResponse> slice = bakeryJdbcRepository.findAll(command);
         return cursorService.getCursorResponse(slice);
+    }
+
+    public BakeryDetailResponse getBakery(Long bakeryId) {
+        Bakery bakery = bakeryJdbcRepository.findById(bakeryId)
+                .orElseThrow(() -> new BreadFeetBusinessException(ErrorCode.BAKERY_NOT_FOUND));
+        return BakeryDetailResponse.from(bakery);
     }
 }
