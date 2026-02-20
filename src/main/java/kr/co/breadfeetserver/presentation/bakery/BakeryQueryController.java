497f03573ba3edd71c40707922b6c0509a61cb36
wlgns12370 <knu12370@knu.ac.kr>
2026-02-20 11:35:28 +0900

    fix: Path 기반 조회로 변경

--- src/main/java/kr/co/breadfeetserver/presentation/bakery/BakeryQueryController.java
+++ src/main/java/kr/co/breadfeetserver/presentation/bakery/BakeryQueryController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,10 +39,10 @@ public class BakeryQueryController {
                         response));
     }
 
-    @GetMapping
+    @GetMapping("/{bakeryId}")
     public ResponseEntity<ApiResponseWrapper<BakeryDetailResponse>> getBakeryDetail(
             @Memberid Long memberId,
-            @RequestParam Long bakeryId
+            @PathVariable Long bakeryId
     ) {
         BakeryDetailResponse response = bakeryQueryService.getBakery(memberId, bakeryId);
 
